e5b680d32ccc2864b6a43ce54e49201def938b0c
LeonaRaging <97384817+LeonaRaging@users.noreply.github.com>
2022-09-01 13:44:05 +0700

    Update qos.cpp

--- qos.cpp
+++ qos.cpp
@@ -6,25 +6,23 @@ using namespace std;
 #define ll long long
 #define pb push_back
 #define db(val) "[" #val " = " << (val) << "] "
-const ll mod = 1e9 + 7;
-const int maxn = 1e3 + 4;
-const int INF = 1e9;
-const int N = 105;
 
-int n, m, t, k, dis[maxn], dp[maxn][N], cnt[maxn];
-vector<pair<int,int>> adj[maxn];
-vector<int> res;
+const ll mod = 1e9 + 7;
+const int maxn = 1e5 + 4;
+const int INF = 1e9;
+
+int n, m, t, k, l = INF, dis[maxn], dp[maxn][105];
+vector<pair<int,int>> adj[maxn], g[maxn];
 
 void dijkstra(int src) {
-    memset(dis, 63, sizeof dis);
-    dis[src] = 0;
     using T = pair<int,int>;
     priority_queue<T, vector<T>, greater<T>> pq;
-    pq.push({0, src});
+    memset(dis, 63, sizeof dis);
+    dis[src] = 0; pq.push({0, src});
     while (!pq.empty()) {
         int d, u; tie(d, u) = pq.top(); pq.pop();
         if (dis[u] != d) continue;
-        for (auto it : adj[u]) {
+        for (auto it : g[u]) {
             int v, w; tie(v, w) = it;
             if (dis[u] + w < dis[v]) {
                 dis[v] = dis[u] + w;
@@ -34,63 +32,66 @@ void dijkstra(int src) {
     }
 }
 
-int Solve(int u, int l) {
-    if (l < dis[u]) return 0;
-    if (dp[u][l - dis[u]] != -1)
-        return dp[u][l - dis[u]];
+int Solve(int u, int p) {
+    if (p + dis[u] > l || p < 0) return 0;
+    if (dp[u][p] != -1)
+        return dp[u][p];
     int res = 0;
     for (auto it : adj[u]) {
         int v, w; tie(v, w) = it;
-        res += Solve(v, l - w);
-        res = min(res, INF);
+        res += Solve(v, p  - w + dis[u] - dis[v]);
+        res = min(res, k);
     }
-    return dp[u][l - dis[u]] = res;
-}
-
-void query(int u, int num) {
-    res.pb(u);
-    if (u == t) return;
-    for (auto it : adj[u]) {
-        int v, w; tie(v, w) = it;
-        if (cnt[v] >= num) {
-            query(v, num);
-            return;
-        }
-        num -= cnt[v];
-    }
+    return dp[u][p] = res;
 }
 
 int main()
 {
-    // ios::sync_with_stdio(0);
-    // cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     //freopen(".INP", "r", stdin);
     //freopen(".OUT", "w", stdout);
     cin >> n >> m >> t >> k;
-    int cmin = INF;
     for (int i = 1; i <= m; i++) {
         int u, v, w; cin >> u >> v >> w;
         adj[u].pb({v, w});
-        adj[v].pb({u, w});
-        cmin = min(cmin, w);
+        g[v].pb({u, w});
+        l = min(l, w);
     }
-    dijkstra(t);
-    memset(dp, -1, sizeof dp);
-    dp[t][0] = 1;
-    int num = 0;
-    for (int i = 0; i <= cmin; i++) {
-        num += Solve(1, i + dis[1]);
-        num = min(num, INF);
-    }
-    if (num < k) return cout << -1, 0;
     for (int i = 1; i <= n; i++)
         sort(adj[i].begin(), adj[i].end());
-    for (int i = 1; i <= n; i++)
-        for (int j = 0; j <= cmin; j++) {
-            cnt[i] += dp[i][j];
-            cnt[i] = min(cnt[i], INF);
+    dijkstra(t);
+    if (dis[1] == dis[0])
+        return cout << -1, 0;
+    l += dis[1];
+    memset(dp, -1, sizeof dp);
+    dp[t][0] = 1;
+    int tot = 0;
+    for (int p = 0; p <= l - dis[1]; p++) {
+        tot += Solve(1, p);
+        tot = min(tot, k);
+    }
+    if (tot < k) return cout << -1, 0;
+    vector<int> res;
+    int u = 1;
+    while (true) {
+        res.pb(u);
+        if (u == t && k == 1) break;
+        for (auto it : adj[u]) {
+            int v, w; tie(v, w) = it;
+            if (dis[v] == dis[0]) continue;
+            int tot = 0;
+            for (int p = 0; p <= l - w - dis[v]; p++) {
+                tot += Solve(v, p);
+                tot = min(tot, k);
+            }
+            if (tot >= k) {
+                u = v; l -= w;
+                break;
+            }
+            else k -= tot;
         }
-    query(1, k);
+    }
     cout << res.size() << '\n';
     for (auto i : res) cout << i << ' ';
 }
