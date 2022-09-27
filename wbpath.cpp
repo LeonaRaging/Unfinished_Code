0e68ce26fad39d7ffe64da8618da48e772eccdb2
LeonaRaging <97384817+LeonaRaging@users.noreply.github.com>
2022-09-27 14:26:37 +0700

    Update wbpath.cpp

--- wbpath.cpp
+++ wbpath.cpp
@@ -15,7 +15,7 @@ const int N = 105;
 int r, c, f[N][N][4 * N], trace[N][N][4 * N];
 int dx[] = {-1, 1, 0, 0};
 int dy[] = { 0, 0,-1, 1};
-char dire[] = {'U', 'D', 'L', 'R'};
+char dire[] = {'D', 'U', 'R', 'L'};
 string grid[N];
 
 bool ok(int i, int j) {
@@ -31,43 +31,41 @@ void Solve() {
     q.push({x, y, d});
     while (!q.empty()) {
         int i, j, diff; tie(i, j, diff) = q.front(); q.pop();
-        // cout << i << ' ' << j << ' ' << diff << '\n';
         for (int p = 0; p < 4; p++) {
             int x = i + dx[p], y = j + dy[p];
             if (!ok(x, y)) continue;
             int nxt = diff + (grid[x][y] == 'W' ? 1 : -1);
-            // if (i == 1 && j == 1 && diff == 7) clog << x << ' ' << y << ' ' << nxt << '\n';
             if (nxt < 0 || nxt > 2 * (r + c)) continue;
             if (f[i][j][diff] + 1 < f[x][y][nxt]) {
                 f[x][y][nxt] = f[i][j][diff] + 1;
-                trace[x][y][nxt] = p;
+                trace[x][y][nxt] = p ^ 1;
                 q.push({x, y, nxt});
             }
         }
     }
     if (f[s][t][r + c] == f[0][0][0])
         return cout << -1 << '\n', void();
-    vector<char> res;
+    string res = "";
     int diff = r + c;
     while (s != x || t != y) {
-        // clog << s << ' ' << t << '\n';
         int p = trace[s][t][diff];
-        res.pb(dire[trace[s][t][diff]]);
-        s += dx[p]; t += dy[p];
+        res += dire[trace[s][t][diff]];
         diff -= (grid[s][t] == 'W' ? 1 : -1);
+        s += dx[p]; t += dy[p];
     }
-    cout << f[s][t][r + c] << ' ';
-    for (auto i : res) cout << i << ' ';
-    cout << '\n';
+    reverse(res.begin(), res.end());
+    cout << res.size() + 1 << ' ' << res << '\n';
 }
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    //freopen(".INP", "r", stdin);
-    //freopen(".OUT", "w", stdout);
+    // freopen("inputf.IN", "r", stdin);
+    // freopen("outputf.OUt", "w", stdout);
     cin >> r >> c;
+    if (r > 100 && c > 100)
+        return cout << "can do yet", 0;
     for (int i = 1; i <= r; i++) {
         cin >> grid[i];
         grid[i] = ' ' + grid[i];
